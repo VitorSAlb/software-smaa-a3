f7f9e463f6e1011a8701c21321a20df4138b6efc
VitorSAlb <vitor.s.albq@gmail.com>
2024-05-23 10:00:47 -0300

    :wq
    ad

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import App from './app/App.jsx'
 import Home from './pages/Home/Home.jsx'
